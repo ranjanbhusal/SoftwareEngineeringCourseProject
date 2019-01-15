7dc89e6be301ccea6ff033152ea0cd5ec4383b2d
ranjan <ranjanbhusal@gmail.com>
2019-01-14 21:56:10 -0600

    new Entity class added

--- mumSched/src/main/webapp/static/js/myScripts.js
+++ mumSched/src/main/webapp/static/js/myScripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-	
 	var index = 0;
 	var courses = {};
 	var first = [];
@@ -9,21 +8,13 @@ $(document).ready(function(){
 	var fourth = [];
 
 	$("#updateID").click(function() {
-		
 		$("ul.entryAdded").hide(3000);
-				
-//		setTimeout(function () {
-//			$("p.entryAdded").css("diplay": "show");
-//		}, 5000);
 	});
 	
 	$('table tr.courseTable').on('click', function() {
 		
 		$(this).toggleClass('selected');
-		
 		var $tds = $(this).find('td');
-		
-
 		
 		myfunc();  
 		
@@ -39,14 +30,9 @@ $(document).ready(function(){
 				}
 				console.log(firstCourse);
 				courses.first = firstCourse;
-//			    $.each($tds, function(i, item) {
-//			    	console.log(item.innerHTML);
-//			    	first.push(item.innerHTML);
-//			    });				
 			}
 		
 			else if (index === 2) {
-				
 				var secondCourse = {
 						courseTitle: $tds[0].innerHTML,
 						courseCode: $tds[1].innerHTML,
@@ -55,15 +41,9 @@ $(document).ready(function(){
 				}
 				console.log(secondCourse);
 				courses.second = secondCourse;
-				
-//			    $.each($tds, function(i, item) {
-//			    	console.log(item.innerHTML);
-//			    	second.push(item.innerHTML);
-//			    });				
 			}
 		
 			else if (index === 3) {
-				
 				var thirdCourse = {
 						courseTitle: $tds[0].innerHTML,
 						courseCode: $tds[1].innerHTML,
@@ -72,15 +52,9 @@ $(document).ready(function(){
 				}
 				console.log(thirdCourse);
 				courses.third =  thirdCourse;
-				
-//			    $.each($tds, function(i, item) {
-//			    	console.log(item.innerHTML);
-//			    	third.push(item.innerHTML);
-//			    });				
 			}
 			
 			else {
-				
 				var fourthCourse = {
 						courseTitle: $tds[0].innerHTML,
 						courseCode: $tds[1].innerHTML,
@@ -89,42 +63,10 @@ $(document).ready(function(){
 				}
 				console.log(fourthCourse);
 				courses.fourth = fourthCourse;
-				
-//				 $.each($tds, function(i, item) {
-//				    	console.log(item.innerHTML);
-//				    	fourth.push(item.innerHTML);
-//				 });	
 			}
-		
 		}
-	    
-
-//		var courses = {
-//		    	"firstCourse": first,	
-//		    	"secondCourse": second, 	
-//		    	"thirdCourse": third,
-//		    	"fourthCourse": fourth
-//		 };	    
-//		console.log("finally" + courses.fourthCourse[1]);
-//	    console.log("first"  +courses.firstCourse[1]);
-//		
-		    
-		
-		
-		
-//	    for (var key in courses) {
-//	    	courses[key] = values; 
-//		    console.log(courses[key]);
-//		}
-//	    
-	    
-
-	    
 	});
 	
-//	var dataForAjax = {
-//			courses: courses
-//	}
 
 	$("#jsBtn").click(function() {
 		console.log("button clicked");
@@ -142,18 +84,5 @@ $(document).ready(function(){
 	         }
 	     });
 		 return false;
-
 	});
-	
-//	$("tr.courseTable").click(function() {
-//		$(this).toggleClass('selected');
-//		
-//	});
-	
-//	var Row = document.getElementById("courseTable");
-//	var Cells = Row.getElementsByTagName("td");
-//	alert(Cells[0].innerText);
-//	alert(Cells[1].innerText);
-	
-	
 });	
